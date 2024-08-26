48521a0c160ee19c4653c73c83c9ab05ebb0e06f
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-26 07:19:56 -0300

    feat: TokenVO class creation

---
+++ src/main/java/com/eurofarma/euforma/security/vo/TokenVO.java
@@ -0,0 +1,96 @@
+package com.eurofarma.euforma.security.vo;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenVO {
+
+	private String email;
+	private Boolean authenticated;
+	private Date created;
+	private Date expiration;
+	private String accessToken;
+	private String refreshToken;
+
+	public TokenVO() {
+	}
+
+	public TokenVO(String email, Boolean authenticated, Date created, Date expiration, String accessToken,
+			String refreshToken) {
+
+		this.email = email;
+		this.authenticated = authenticated;
+		this.created = created;
+		this.expiration = expiration;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getAuthenticated() {
+		return authenticated;
+	}
+
+	public void setAuthenticated(Boolean authenticated) {
+		this.authenticated = authenticated;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accessToken, authenticated, created, email, expiration, refreshToken);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TokenVO other = (TokenVO) obj;
+		return Objects.equals(accessToken, other.accessToken) && Objects.equals(authenticated, other.authenticated)
+				&& Objects.equals(created, other.created) && Objects.equals(email, other.email)
+				&& Objects.equals(expiration, other.expiration) && Objects.equals(refreshToken, other.refreshToken);
+	}
+
+}
