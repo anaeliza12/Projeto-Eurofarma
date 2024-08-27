4a76445cbca74c130f518578f1a9ec39b1b6d12b
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-27 08:32:18 -0300

    feat: creating TokenFilter class

---
+++ src/main/java/com/eurofarma/euforma/security/jwt/TokenFilter.java
@@ -0,0 +1,39 @@
+package com.eurofarma.euforma.security.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class TokenFilter extends GenericFilterBean  {
+	
+	@Autowired
+	private TokenProvider tokenProvider;
+
+	public TokenFilter(TokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String token = tokenProvider.resolveToken((HttpServletRequest) request);
+		if (token != null && tokenProvider.validateToken(token)) {
+			Authentication auth = tokenProvider.getAuthentication(token);
+			if(auth != null) {				
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		chain.doFilter(request, response);		
+	}
+
+}
