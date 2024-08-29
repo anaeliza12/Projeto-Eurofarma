3f737adf1887d872b3f50762c0c46d4f13a74f85
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-29 08:31:48 -0300

    feat: add SQL scripts to feed the tables

---
+++ SQL scripts/Department Table.sql
@@ -0,0 +1,38 @@
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Recursos Humanos ");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Segurança da Informação");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Pesquisa e Desenvolvimento");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Controle de Qualidade");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Produção e Manufatura");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Segurança do Trabalho");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Jurídico e Compliance");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Finanças e Contabilidade");
+
+
+INSERT INTO eurofarma.departamentos
+VALUES (null, "Vendas e Marketing");
+
+
+
+
