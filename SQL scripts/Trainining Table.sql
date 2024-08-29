0fa547a6b7e5b6599f00a0191e4a24485001da7a
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-29 11:14:11 -0300

    feat: feeding Training table with script SQL

---
+++ SQL scripts/Trainining Table.sql
@@ -0,0 +1,6 @@
+INSERT INTO eurofarma.treinamentos
+VALUES (null, "Atendimento ao Cliente de Excelência", 9, "1h10", '2024-12-04', '16:45', "Melhores práticas para oferecer um atendimento ao cliente excepcional, incluindo técnicas de comunicação e resolução de problemas.", "Presencial", "Auditório");
+
+INSERT INTO eurofarma.treinamentos
+VALUES (null, "Marketing Digital e Redes Sociais", 9, "2h45", '2024-08-29', '15:30', "Treinamento focado em desenvolver habilidades de liderança, incluindo técnicas de motivação, comunicação eficaz e gestão de equipes. Ideal para líderes atuais e futuros.", "On-line", "Sala 11");
+
