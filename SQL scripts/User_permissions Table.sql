5bff4e3063392af16512fdc03ef62271ef029b07
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-29 11:14:57 -0300

    feat: feeding User_permissions table with script SQL

---
+++ SQL scripts/User_permissions Table.sql
@@ -0,0 +1,18 @@
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (1,2);
+
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (2,2);
+
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (3,2);
+
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (4,2);
+
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (5,1);
+
+INSERT INTO eurofarma.usuario_permissoes
+VALUES (6,1);
+
