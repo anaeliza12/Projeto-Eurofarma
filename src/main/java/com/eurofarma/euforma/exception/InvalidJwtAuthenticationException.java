71f810d16416ea02215d652d9919c0f7cc00b8ab
Ana Eliza <anaelizaperobelli@gmail.com>
2024-09-07 22:35:10 -0300

    fix: exceptions classes changes

--- src/main/java/com/eurofarma/euforma/exception/InvalidJwtAuthenticationException.java
+++ src/main/java/com/eurofarma/euforma/exception/InvalidJwtAuthenticationException.java
@@ -5,10 +5,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class InvalidJwtAuthenticationException extends AuthenticationException {
-
+public class InvalidJwtAuthenticationException extends AuthenticationException{
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	public InvalidJwtAuthenticationException(String ex) {
 		super(ex);
 	}
--- src/main/java/com/eurofarma/euforma/exception/handler/CustumizedRespondeEntityExceptionHanldler.java
+++ src/main/java/com/eurofarma/euforma/exception/handler/CustumizedRespondeEntityExceptionHanldler.java
@@ -1,6 +1,6 @@
 package com.eurofarma.euforma.exception.handler;
 
-import java.nio.file.AccessDeniedException;
+import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.eurofarma.euforma.exception.ExceptionResponse;
 import com.eurofarma.euforma.exception.InvalidJwtAuthenticationException;
 
 @ControllerAdvice
 @RestController
-public class CustumizedRespondeEntityExceptionHanldler {
-
-	@ExceptionHandler(AccessDeniedException.class)
-	public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
-		return new ResponseEntity<>("Você não tem permissão para acessar esse recurso.", HttpStatus.FORBIDDEN);
-	}
+public class CustumizedRespondeEntityExceptionHanldler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(InvalidJwtAuthenticationException.class)
-	public final ResponseEntity<String> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex,
+			WebRequest request) {
 
-		return new ResponseEntity<>("JWT inválido.", HttpStatus.FORBIDDEN);
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
-
 }
