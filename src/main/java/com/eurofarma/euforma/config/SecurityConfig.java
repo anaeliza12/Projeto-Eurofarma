c0602d5c72b31ff43c92148e47bbf6976a6101d2
Ana Eliza <anaelizaperobelli@gmail.com>
2024-09-07 22:28:23 -0300

    fix: security class changes

--- src/main/java/com/eurofarma/euforma/config/SecurityConfig.java
+++ src/main/java/com/eurofarma/euforma/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.eurofarma.euforma.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,18 +11,34 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.eurofarma.euforma.security.jwt.TokenFilter;
+import com.eurofarma.euforma.security.jwt.JwtTokenFilter;
 import com.eurofarma.euforma.security.jwt.TokenProvider;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-	
 	@Autowired
 	private TokenProvider tokenProvider;
+
+	@Bean
+	public PasswordEncoder passwordEnconder() {
+		Map<String, PasswordEncoder> enconders = new HashMap<>();
+		Pbkdf2PasswordEncoder pbkdf2Enconder = new Pbkdf2PasswordEncoder("", 8, 185000,
+				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+		enconders.put("pbkdf2", pbkdf2Enconder);
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", enconders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Enconder);
+
+ 		return passwordEncoder;
+	}
 	
 	@Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) 
@@ -29,19 +48,19 @@ public class SecurityConfig {
 	
 	@Bean
 	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		 TokenFilter customFilter = new TokenFilter(tokenProvider);
-		    return http
-		        .httpBasic(basic -> basic.disable())
-		        .csrf(csrf -> csrf.disable())
-		        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		        .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
-		        .authorizeHttpRequests(auth -> 
-		            auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/auth/refresh/**",
-		                    "/api-docs/**", "/api-eurofarma/v1/login").permitAll()		
-			            .requestMatchers("api-eurofarma/v1/admin/**").hasRole("ADMIN")
-			            .requestMatchers("api-eurofarma/v1/user/**").hasRole("USER")
-		                .requestMatchers("/users").denyAll())		
-		        .cors(cors -> {})
-		        .build();		
+		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+		return http
+		.httpBasic(basic -> basic.disable())
+		.csrf(csrf -> csrf.disable())
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
+		.authorizeHttpRequests(auth -> 
+		auth.requestMatchers("/swagger-ui/**","/v3/api-docs/**", "/api-eurofarma/auth/v1/refresh/**",
+				"/api-docs/**", "/api-eurofarma/auth/v1/login").permitAll()		
+				.requestMatchers("/api/**").authenticated() 
+				.requestMatchers("/users").denyAll())
+				.cors(cors -> {})
+				.build();
 	}
+
 }
