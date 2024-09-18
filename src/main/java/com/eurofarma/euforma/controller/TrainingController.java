ae28c3f9cb8b95c331fe7076344e1a45c49f7963
Ana Eliza <anaelizaperobelli@gmail.com>
2024-09-18 14:26:22 -0300

    feat: adding swagger into trainingController

--- src/main/java/com/eurofarma/euforma/controller/TrainingController.java
+++ src/main/java/com/eurofarma/euforma/controller/TrainingController.java
@@ -17,8 +17,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.eurofarma.euforma.entities.Training;
 import com.eurofarma.euforma.entities.UserTraining;
 import com.eurofarma.euforma.services.TrainingService;
+import com.github.anaeliza12.FirstProject.data.vo.v1.PersonVO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Training", description = "Endpoints for Managing Trainings")
 @RequestMapping(value = "/api-eurofarma/training")
 public class TrainingController {
 
@@ -27,12 +36,32 @@ public class TrainingController {
 
 	@SuppressWarnings("rawtypes")
 	@GetMapping(value = "/v1/all")
+	@Operation(summary = "Finds all Trainings", description = "A method that brings all trainings from the database", tags = {"Training"},
+	responses = { 
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = { 
+							@Content(
+									mediaType = "application/json",
+									array =  @ArraySchema(schema = @Schema(implementation = Training.class)))
+					}),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
 	public ResponseEntity findAll() {
 		var trainings = service.findAll();
 		return ResponseEntity.ok(trainings);
 	}
 
 	@PostMapping(value = "/v1/subscribe")
+	@Operation(summary = "Subscribe Training", description = "A method that subscribe the actual user by passing JWT access token on Headers and the training in a JSON representation of the training! ", tags = {"Training"},
+	   responses =  {@ApiResponse( description = "Success" ,responseCode = "200", 
+	   content = {
+			   @Content( mediaType = "application/json",
+					   schema = @Schema(implementation = Training.class))
+	   }),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
 	public ResponseEntity subscribe(@RequestBody Training training) {
 		var subscribe = service.subscribe(training);
 		if (subscribe == null) {
@@ -42,19 +71,46 @@ public class TrainingController {
 	}
 
 	@PostMapping(value = "/v1/create")
+	@Operation(summary = "Create Training", description = "A method that create a training by passing in a JSON representation of the training! ", tags = {"Training"},
+	   responses =  {@ApiResponse( description = "Success" ,responseCode = "200", 
+	   content = {
+			   @Content( mediaType = "application/json",
+					   schema = @Schema(implementation = Training.class))
+	   }),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
 	public ResponseEntity create(@RequestBody Training training) {
 		var newTraining = service.insert(training);
 		URI uri = ServletUriComponentsBuilder.fromPath("/{ex}").buildAndExpand(training.getId()).toUri();
 		return ResponseEntity.created(uri).body(training);
 	}
-
+	
+	
 	@DeleteMapping(value = "/v1/delete/{id}")
+	@Operation(summary = "Delete Training", description = "A method that delete a training by passing the given id in path variable! ", tags = {"Training"},
+	   responses =  {@ApiResponse( description = "Success" ,responseCode = "200", 
+	   content = {
+			   @Content( mediaType = "application/json",
+					   schema = @Schema(implementation = Training.class))
+	   }),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "No content", responseCode = "204", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
 	public ResponseEntity delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().header("Status", "User-deleted").build();
 	}
 
 	@PutMapping(value = "/v1/update/{id}")
+	@Operation(summary = "Delete Training", description = "A method that delete a training by passing the given id in path variable! ", tags = {"Training"},
+	   responses =  {@ApiResponse( description = "Success" ,responseCode = "200", 
+	   content = {
+			   @Content( mediaType = "application/json",
+					   schema = @Schema(implementation = Training.class))
+	   }),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "No content", responseCode = "204", content = @Content),
+		@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
 	public ResponseEntity update(@RequestBody Training training, @PathVariable Long id) {
 		var newTraining = service.update(training, id);
 		return ResponseEntity.ok(newTraining);
