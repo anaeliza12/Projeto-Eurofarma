efb0a6f29a0644042c762a1e3ce5724704701a61
Ana Eliza <anaelizaperobelli@gmail.com>
2024-09-20 23:20:51 -0300

    fix: adding more information at authentication class

--- src/main/java/com/eurofarma/euforma/controller/AuthController.java
+++ src/main/java/com/eurofarma/euforma/controller/AuthController.java
@@ -11,22 +11,32 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eurofarma.euforma.entities.Training;
 import com.eurofarma.euforma.security.vo.AccountCredencialsVO;
+import com.eurofarma.euforma.security.vo.TokenVO;
 import com.eurofarma.euforma.services.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api-eurofarma/auth")
-@Tag(name = "Authentication Endpoint")
+@Tag(name = "Authentication Endpoint",  description = "Endpoints for authenticate an user")
 public class AuthController {
 
 	@Autowired
 	AuthService authService;
 
 	@SuppressWarnings("rawtypes")
-	@Operation(summary = "Authenticates an user and returns a token")
+	@Operation(summary = "Authenticates an user and returns a token", description = "A method that return a JWT token", tags = {
+			"Training" }, responses = { @ApiResponse(description = "Success", responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TokenVO.class)))}),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+			})
 	@PostMapping(value = "/v1/login")
 	public ResponseEntity signIn(@RequestBody AccountCredencialsVO data) {
 		if (checkIfParamIsNotNull(data))
@@ -38,7 +48,11 @@ public class AuthController {
 	}
 
 	@SuppressWarnings("rawtypes")
-	@Operation(summary = "Refresh token for authenticated user and returns a token")
+	@Operation(summary = "Refresh token for authenticated user and returns a token", description = "A method that return a refresh token", tags = {
+	"Training" }, responses = { @ApiResponse(description = "Success", responseCode = "200", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TokenVO.class)))}),
+	@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	@PutMapping(value = "v1/refresh/{email}")
 	public ResponseEntity refreshToken(@PathVariable String email,
 			@RequestHeader("Authorization") String refreshToken) {
