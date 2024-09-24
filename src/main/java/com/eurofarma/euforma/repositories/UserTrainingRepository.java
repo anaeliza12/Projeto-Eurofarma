205d4d88fa3cbdf71e2b93774bf3b6fa45935dbf
Ana Eliza <anaelizaperobelli@gmail.com>
2024-09-24 20:52:27 -0300

    fix: changing id type from UserTrainingRepository

--- src/main/java/com/eurofarma/euforma/repositories/UserTrainingRepository.java
+++ src/main/java/com/eurofarma/euforma/repositories/UserTrainingRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eurofarma.euforma.entities.UserTraining;
+import com.eurofarma.euforma.entities.pk.UserTrainingPK;
 
 @Repository
-public interface UserTrainingRepository extends JpaRepository<UserTraining, Long> {
+public interface UserTrainingRepository extends JpaRepository<UserTraining, UserTrainingPK> {
 
 }
